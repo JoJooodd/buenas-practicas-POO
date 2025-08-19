b1e79a179f882ddec1443280c37dc8e84faa9fc5
Josué Bautista <josubauti@outlook.com>
2025-08-19 09:25:49 -0600

    refator: apply abstraction at the function level

--- post-app/src/app/api/route.ts
+++ post-app/src/app/api/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import postgres from "postgres";
 import PostRegister from "../utils/post-register";
 
 
@@ -12,11 +11,7 @@ export async function POST(request:NextRequest) {
         const data = await request.json()
 
         const register = new PostRegister()
-        await register.run()
-
-        isValidPost(data);
-
-        await savePost(data);
+        await register.run(data.id, data.title, data.description, data.author)
 
         return NextResponse.json({
             message: 'Data saved successfully in post'
@@ -27,33 +22,4 @@ export async function POST(request:NextRequest) {
             message: 'Failed to save data'
         }, {status:500})
     }
-}
-
-
-function isValidPost(data:any):void {
-    if (data && data.id && data.title && data.description && data.author) {
-
-        if (typeof data.id !== "number") {
-            throw new Error('Invalid id')
-        }
-
-        if (typeof data.title !== "string" || data.title.length >= 15) {
-            throw new Error('Invalid title')
-        }
-
-        if (typeof data.description !== "string" || 
-            data.description.length >= 40) {
-            throw new Error('Invalid description')
-        }
-
-        if (typeof data.author !== "string" || data.author.length <= 1) {
-            throw new Error('Invalid author')
-        }
-    }
-}
-
-async function savePost(data:any): Promise<void> {
-    const connectionString = 'postgresql://postgres.ekyyyptzflltjjbgjdrm:Josue321@aws-1-us-east-2.pooler.supabase.com:6543/postgres';
-    const sql = postgres(connectionString);
-    await sql `INSERT INTO posts (title, description, author) VALUES (${data.title},  ${data.description}, ${data.author});`
 }
--- post-app/src/app/utils/post-register.ts
+++ post-app/src/app/utils/post-register.ts
@@ -1,8 +1,45 @@
 '/src/utils/post-register.ts';
+import postgres from "postgres";
+
+
 
 
 export default class PostRegister {
-    public async run() {
-        
-    } 
+
+    constructor(){}
+
+    public async run(id: number, title: string, description: string, author: string) {
+        isValidPost(id, title, description, author);
+        await savePost(title, description, author);
+    }
+}
+
+
+function isValidPost(id: number, title: string, description: string, author: string):void {
+    if (id && title && description && author) {
+
+        if (typeof id !== "number") {
+            throw new Error('Invalid id')
+        }
+
+        if (typeof title !== "string" || title.length >= 15) {
+            throw new Error('Invalid title')
+        }
+
+        if (typeof description !== "string" || 
+            description.length >= 40) {
+            throw new Error('Invalid description')
+        }
+
+        if (typeof author !== "string" || author.length <= 1) {
+            throw new Error('Invalid author')
+        }
+    }
+}
+
+
+async function savePost(title: string, description: string, author: string): Promise<void> {
+    const connectionString = 'postgresql://postgres.ekyyyptzflltjjbgjdrm:Josue321@aws-1-us-east-2.pooler.supabase.com:6543/postgres';
+    const sql = postgres(connectionString);
+    await sql `INSERT INTO posts (title, description, author) VALUES (${title},  ${description}, ${author});`
 }
